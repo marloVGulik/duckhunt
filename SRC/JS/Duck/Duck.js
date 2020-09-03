3d3cdc9a68549b087d492aa434e4f96522b08810
Marlo van Gulik <mjvangulik208@gmail.com>
2020-09-03 15:13:40 +0200

    Duck done, all crashes resolved and added scoreboard

--- SRC/JS/Duck/Duck.js
+++ SRC/JS/Duck/Duck.js
@@ -1,23 +1,32 @@
 class Duck {
     duckHTML = null;
+    duckHTMLId = 0;
     duckPos = 0;
     updateInterval = null;
 
     constructor() {
         var body = document.getElementsByTagName("body")[0];
-        this.duckPos = new Vec2(screen.width / 2, screen.height / 2);
+        this.duckPos = new Vec2(screen.width / 2 - 125, screen.height / 2 - 125);
         console.log(this.duckPos);
+
+        do {
+            this.duckHTMLId = "Duck" + Math.floor(Math.random() * 1000);
+        } while(document.getElementById(this.duckHTMLId));
 
         this.duckHTML = document.createElement("img");
         this.duckHTML.src = "SRC/Images/duck.gif";
         this.duckHTML.classList.add("duck");
+        this.duckHTML.id = this.duckHTMLId;
         this.duckHTML.style.left = this.duckPos.x + "px";
         this.duckHTML.style.top = this.duckPos.y + "px";
+        this.duckHTML.onclick = function() {
+            updateGame(1, this.id);
+        };
         body.appendChild(this.duckHTML);
 
         var updateFun = this.update;
         // this.updateInterval = setInterval(function(duckPos, duckHTML){updateFun(duckPos, duckHTML)}, 15, this.duckPos, this.duckHTML);
-        this.updateInterval = setInterval(function(duck){updateFun(duck)}, 1500, this);
+        this.updateInterval = setInterval(function(duck){updateFun(duck)}, 15, this);
     }
 
     // update(duckPos, duckHTML) {
@@ -26,23 +35,35 @@ class Duck {
         var count = 0;
         do {
             count++;
+
+
             action = actions[Math.floor(Math.random() * actions.length)];
-            for(var i = 0; i < 25; i++) {
-                actions[i + 8] = action;
-            }
+            // for(var i = 0; i < 25; i++) {
+            //     actions[i + 8] = action;
+            // }
             if(duck.duckPos == undefined) {
                 duck.duckPos = new Vec2(screen.width / 2, screen.height / 2);
                 console.error("Duck position is undefined");
             }
-            duck.duckPos = duck.duckPos.add(new Vec2(action.multiply(screen.width / 50).x, action.multiply(screen.width / 20).y));
+            duck.duckPos = duck.duckPos.add(new Vec2(action.multiply(10).x, action.multiply(10).y));
 
-            console.error(duck.duckPos.outside(new Vec2(0, 0), new Vec2(screen.width, screen.height)));
+            // console.error(duck.duckPos.outside(new Vec2(0, 0), new Vec2(screen.width, screen.height)));
 
-        } while(!duck.duckPos.outside(new Vec2(0, 0), new Vec2(screen.width, screen.height)) && count < 3000);
+        } while(!duck.duckPos.outside(new Vec2(0, 0), new Vec2(screen.width - 125, screen.height - 125)) && count < 1000);
 
-        console.log("Adding " + action.x + ", " + action.y + " to " + duck.duckPos.x + ", " + duck.duckPos.y);
+        if(count == 1000) {
+            console.error("Considering duck to be in illegal position! Resetting to default ");
+            duck.duckPos = new Vec2(screen.width / 2 - 125, screen.height / 2 - 125);
+            return;
+        }
+
+        // console.log("Adding " + action.x + ", " + action.y + " to " + duck.duckPos.x + ", " + duck.duckPos.y);
         duck.duckHTML.style.left = duck.duckPos.x + "px";
         duck.duckHTML.style.top = duck.duckPos.y + "px";
+    }
+
+    isMyId(id) {
+        return id == this.duckHTMLId;
     }
 
     destroy() {
@@ -50,8 +71,6 @@ class Duck {
         clearInterval(this.updateInterval);
     }
 }
-
-var last = 0;
 
 var actions = [
     new Vec2(0, 1),
--- SRC/JS/main.js
+++ SRC/JS/main.js
@@ -1,4 +1,40 @@
-var duck = new Duck;
+var ducks = [
+    
+];
 
-console.log(duck);
+for(var i = 0; i < 100; i++) {
+    ducks.push(new Duck());
+}
+
+// console.log(duck);
 // duck.destroy();
+
+
+var gameCounter = 0;
+var scoreCounter = 0;
+
+var hitsHTML = document.getElementById("hits");
+var missHTML = document.getElementById("miss");
+
+hitsHTML.innerHTML = "0 hits";
+missHTML.innerHTML = "0 miss";
+
+function updateGame(hit, id) {
+    gameCounter++;
+    scoreCounter += hit;
+
+    for(var i = 0; i < ducks.length; i++) {
+        if(ducks[i].isMyId(id)) {
+            ducks[i].destroy();
+            ducks.splice(i, 1);
+        }
+    }
+
+    hitsHTML.innerHTML = scoreCounter + " hits";
+    missHTML.innerHTML = scoreCounter - gameCounter + " miss";
+
+    if(gameCounter >= 20) {
+        console.error("restart");
+    }
+    
+}
